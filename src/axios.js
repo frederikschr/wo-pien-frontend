d86a7de1ac7e1a1cf8a259c0baeafa52a2f72499
Frederik Schier <fredi.schier@gmail.com>
2022-07-06 23:07:59 +0200

    baseURL changed#

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://192.168.178.75:5000/'
+axios.defaults.baseURL = '52.59.225.71'
 
 // axios.defaults.baseURL = 'https://wo-pien-backend.herokuapp.com/'
