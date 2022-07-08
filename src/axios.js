11aa6e27d7c7e0ad750cb4b46f09572667b4206b
Frederik Schier <fredi.schier@gmail.com>
2022-07-08 03:08:04 +0200

    https added

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://52.59.225.71'
+axios.defaults.baseURL = 'https://wopien-backend.wopien-backend.cloudns.ph'
 
 // axios.defaults.baseURL = 'https://wo-pien-backend.herokuapp.com/'
